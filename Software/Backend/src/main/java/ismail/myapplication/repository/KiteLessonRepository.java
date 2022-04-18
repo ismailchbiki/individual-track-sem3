98c6d30439ef8d7c5e5c2688a93962789b2a4b23
Ismailchbiki <ismailchbiki.nl@gmail.com>
2022-04-18 15:47:46 +0200

    Integration testing - repository

--- Software/Backend/src/main/java/ismail/myapplication/repository/KiteLessonRepository.java
+++ Software/Backend/src/main/java/ismail/myapplication/repository/KiteLessonRepository.java
@@ -3,8 +3,7 @@ package ismail.myapplication.repository;
 import ismail.myapplication.repository.entity.KiteLesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface KiteLessonRepository extends JpaRepository<KiteLesson, Long> {
-    Optional<KiteLesson> findByType(String type);
+    KiteLesson findByType(String type);
+    boolean existsByType(String type);
 }
--- Software/Backend/src/test/java/ismail/myapplication/repository/KiteLessonRepositoryTest.java
+++ Software/Backend/src/test/java/ismail/myapplication/repository/KiteLessonRepositoryTest.java
@@ -1,148 +1,75 @@
 package ismail.myapplication.repository;
 
 import ismail.myapplication.repository.entity.KiteLesson;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Optional;
+import javax.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class KiteLessonRepositoryTest {
 
     @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
     private KiteLessonRepository kiteLessonRepository;
 
-    @BeforeEach
-    void setUp(){
-        kiteLessonRepository.deleteAll();
-    }
-
     @Test
-    void saveKiteLessonTest_shouldSaveKiteLesson(){
-        assertThat(kiteLessonRepository.findAll().isEmpty());
-        KiteLesson kiteLesson = KiteLesson.builder()
-                .type("repository-test-type")
-                .persons(1)
+    void findByType_shouldReturnKiteLesson_whenItExists() {
+
+        entityManager.persist(KiteLesson.builder()
+                .type("prive")
+                .persons(2)
                 .hours(2D)
-                .price(300D)
-                .build();
+                .price(99.99)
+                .build());
 
-        kiteLessonRepository.save(kiteLesson);
+        KiteLesson actualKiteLesson = kiteLessonRepository.findByType("prive");
 
-        assertThat(kiteLesson.getId()).isPositive();
-    }
-
-    @Test
-    void getKiteLessonByIdTest_shouldReturnKiteLesson(){
-        assertThat(kiteLessonRepository.findAll().isEmpty());
-        KiteLesson kiteLesson = kiteLessonRepository.getById(66L);
-        assertThat(kiteLesson.getId()).isEqualTo(66L);
-    }
-
-    @Test
-    void getKiteLessonByTypeTest_shouldReturnKiteLesson(){
-        assertThat(kiteLessonRepository.findAll().isEmpty());
-
-        //arrange
-        KiteLesson kiteLesson = KiteLesson.builder()
-                .type("private-lesson")
-                .persons(1)
+        assertNotNull(actualKiteLesson.getId());
+        KiteLesson expectedKiteLesson = KiteLesson.builder()
+                .type("prive")
+                .persons(2)
                 .hours(2D)
-                .price(300D)
+                .price(99.99)
                 .build();
-
-        //act
-        kiteLessonRepository.save(kiteLesson);
-        assertThat(kiteLessonRepository.findAll()).hasSize(1);
-
-        //assert
-        assertThat(kiteLesson.getType()).isEqualTo("private-lesson");
+        assertThat(actualKiteLesson)
+                .usingRecursiveComparison()
+                .ignoringExpectedNullFields()
+                .isEqualTo(expectedKiteLesson);
     }
 
     @Test
-    void getAllKiteLessonsTest_shouldReturnAllKiteLessons(){
-        assertThat(kiteLessonRepository.findAll().isEmpty());
+    void findByType_shouldReturnNull_whenKiteLessonNotFound() {
+        KiteLesson actualKiteLesson = kiteLessonRepository.findByType("null");
+        assertNull(actualKiteLesson);
+    }
 
-        KiteLesson kiteLesson1 = KiteLesson.builder()
-                .type("repository-test-type")
-                .persons(1)
+    @Test
+    void existsByType_shouldReturnTrue_whenTypeFound() {
+
+        entityManager.persist(KiteLesson.builder()
+                .type("prive")
+                .persons(2)
                 .hours(2D)
-                .price(300D)
-                .build();
+                .price(99.99)
+                .build());
 
-        KiteLesson kiteLesson2 = KiteLesson.builder()
-                .type("repository-test-type")
-                .persons(1)
-                .hours(2D)
-                .price(300D)
-                .build();
-
-        kiteLessonRepository.save(kiteLesson1);
-        kiteLessonRepository.save(kiteLesson2);
-        assertThat(kiteLessonRepository.findAll()).hasSize(2);
+        boolean actual = kiteLessonRepository.existsByType("prive");
+        assertTrue(actual);
     }
 
     @Test
-    void updateKiteLessonTest_shouldUpdateKiteLesson(){
-        assertThat(kiteLessonRepository.findAll().isEmpty());
-
-        //save kite lesson to update
-        KiteLesson existingKiteLesson = KiteLesson.builder()
-                .type("repository-test-type")
-                .persons(1)
-                .hours(2D)
-                .price(300D)
-                .build();
-        KiteLesson kiteLessonToUpdate = kiteLessonRepository.save(existingKiteLesson);
-
-        //check if saved
-        assertThat(kiteLessonRepository.findAll()).hasSize(1);
-        assertThat(kiteLessonRepository.findById(1L).isPresent());
-
-        //update kite lesson
-        kiteLessonToUpdate.setType("repository-test-type-updated");
-        kiteLessonToUpdate.setPersons(2);
-        kiteLessonToUpdate.setHours(3D);
-        kiteLessonToUpdate.setPrice(400D);
-
-        //save updated kite lesson to db
-        KiteLesson kiteLessonUpdated = kiteLessonRepository.save(kiteLessonToUpdate);
-
-        //check if updated
-        assertThat(kiteLessonUpdated.getType()).isEqualTo("repository-test-type-updated");
-        assertThat(kiteLessonUpdated.getPersons()).isEqualTo(2);
-        assertThat(kiteLessonUpdated.getHours()).isEqualTo(3D);
-        assertThat(kiteLessonUpdated.getPrice()).isEqualTo(400D);
-    }
-
-    @Test
-    void deleteKiteLessonTest_shouldDeleteKiteLesson(){
-        assertThat(kiteLessonRepository.findAll().isEmpty());
-
-        // find and delete kite lesson
-        KiteLesson kiteLesson = kiteLessonRepository.getById(98L);
-        kiteLessonRepository.delete(kiteLesson);
-
-        KiteLesson kiteLessonNull = null;
-
-        Optional<KiteLesson> deletedKiteLesson = kiteLessonRepository.findByType("private");
-
-        if(deletedKiteLesson.isPresent()){
-            kiteLessonNull = deletedKiteLesson.get();
-        }
-
-        assertThat(kiteLessonRepository.findAll().isEmpty());
-        assertThat(kiteLessonNull).isNull();
-    }
-
-    @Test
-    void deleteAllKiteLessonTest_shouldDeleteAllKiteLesson(){
-        assertThat(kiteLessonRepository.findAll().isEmpty());
-        kiteLessonRepository.deleteAll();
-        assertThat(kiteLessonRepository.findAll().isEmpty());
+    void existsByType_shouldReturnFalse_whenTypeNotFound() {
+        boolean actual = kiteLessonRepository.existsByType("null");
+        assertFalse(actual);
     }
 }
