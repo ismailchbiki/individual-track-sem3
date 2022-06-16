f9f72b09745965b2ac1b61f5cc4cd4490ad77fb4
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2022-06-17 00:23:28 +0200

    Documentation - web performance review

--- Software/Backend/src/main/java/ismail/myapplication/config/WebSocketConfig.java
+++ Software/Backend/src/main/java/ismail/myapplication/config/WebSocketConfig.java
@@ -9,11 +9,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    //to establish the connection the server side
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //the client-side connects to this point to connect with the backend
+        //using sockjs
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
+
+    //after the backend processing is done,
+    //pass the messages to a broker,
+    //so that the broker can send the data back to the client,
+    //and the client will be listening to the created broker (topic): chatroom & user to get the data back
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
